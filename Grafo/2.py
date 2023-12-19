4c11a896b53035074fed1ba3b23a8cf7490a5c9b
IzadoraFernandes <izadorasilvams@gmail.com>
2023-12-19 13:25:01 -0300

    mudando estrutura dos dados, de matriz para json

--- Grafo/2.py
+++ Grafo/2.py
@@ -9,7 +9,7 @@ def ler_grafo_do_arquivo(nome_arquivo):
 
     G = nx.Graph()
 
-    # Adiciona as arestas ao grafo
+    # arestas do grafo
     for origem, destinos in data.items():
         for destino, peso in destinos.items():
             G.add_edge(int(origem), int(destino), weight=peso)
@@ -17,14 +17,14 @@ def ler_grafo_do_arquivo(nome_arquivo):
     return G
 
 def caixeiro_viajante_grafo(G):
-    # Inicializar a melhor solução encontrada até agora
+    # Inicializando a melhor solução encontrada até agora
     melhor_caminho = None
     menor_distancia = float('inf')
 
-    # Obter todas as arestas com seus dados
+    #pegando as informações das arestas
     arestas = list(G.edges(data=True))
 
-    # Obter a lista de todos os nós
+    #lista de todos os nós
     nos = list(G.nodes)
 
     # Testar todas as permutações dos índices dos nós
@@ -38,31 +38,31 @@ def caixeiro_viajante_grafo(G):
 
     return G.copy(), melhor_caminho, arestas
 
-# Nome do arquivo JSON contendo a estrutura do grafo
-nome_arquivo_json = 'graph1.json'
 
-# Ler o grafo do arquivo JSON
+nome_arquivo_json = 'graph2.json'
+
+
 grafo = ler_grafo_do_arquivo(nome_arquivo_json)
 
-# Exemplo de uso
+
 G, solucao_grafo, arestas = caixeiro_viajante_grafo(grafo)
 
 # Desenhar o grafo
 pos = nx.circular_layout(G)
 nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=700, node_color='skyblue')
 
-# Destacar o caminho ótimo
+# destacando o melhor caminho
 arestas_destacadas = [(solucao_grafo[i], solucao_grafo[i + 1]) for i in range(len(solucao_grafo) - 1)]
 arestas_destacadas.append((solucao_grafo[-1], solucao_grafo[0]))
 nx.draw_networkx_edges(G, pos, edgelist=arestas_destacadas, edge_color='red', width=2)
 
-# Exibir a distância total no título
+
 distancia_total = sum(arestas[i][2]['weight'] for i in range(len(solucao_grafo) - 1))
 distancia_total += arestas[solucao_grafo[-1]][2]['weight']  # Voltar ao nó inicial
 plt.title(f'Caminho ótimo (Distância Total: {distancia_total})')
 
-# Mostrar o gráfico
+# gráfico
 plt.show()
 
-# Imprimir o melhor caminho no terminal
+#melhor caminho
 print("Melhor caminho:", solucao_grafo)
